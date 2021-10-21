1b1e21b44d22395bb532f5bc380141815d7a4d72
Ruofan He <67919467+ruofan-he@users.noreply.github.com>
2021-10-21 20:32:59 +0900

    register module with adc_threshold IO

--- fpga/rtl/PNR/PNR_register.v
+++ fpga/rtl/PNR/PNR_register.v
@@ -1,13 +1,13 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: Furusawa lab.
+// Engineer: Ruofan He
 // 
 // Create Date: 2021/10/21 18:03:08
 // Design Name: 
 // Module Name: PNR_register
-// Project Name: 
-// Target Devices: 
+// Project Name: redpitaya_PNR
+// Target Devices:
 // Tool Versions: 
 // Description: 
 // 
@@ -33,7 +33,15 @@ module PNR_register(
    input  wire            sys_ren         ,  //!< bus read enable
    output wire [ 32-1: 0] sys_rdata       ,  //!< bus read data
    output wire            sys_err         ,  //!< bus error indicator
-   output wire            sys_ack            //!< bus acknowledge signal
+   output wire            sys_ack         ,  //!< bus acknowledge signal
+   //ADC_threshold for photon number resolving
+   output wire [ 14-1: 0] adc_photon_threshold_1,
+   output wire [ 14-1: 0] adc_photon_threshold_2,
+   output wire [ 14-1: 0] adc_photon_threshold_3,
+   output wire [ 14-1: 0] adc_photon_threshold_4,
+   output wire [ 14-1: 0] adc_photon_threshold_5,
+   output wire [ 14-1: 0] adc_photon_threshold_6,
+   output wire [ 14-1: 0] adc_photon_threshold_7,
     );
 
 reg [32-1:0] _sys_rdata;
@@ -46,11 +54,27 @@ assign sys_ack   = _sys_ack;
 
 
 
-reg [14-1:0] some_reg;
+
+reg [8-1:0] led_reg;
+reg [14-1:0] adc_photon_threshold_1_reg;
+reg [14-1:0] adc_photon_threshold_2_reg;
+reg [14-1:0] adc_photon_threshold_3_reg;
+reg [14-1:0] adc_photon_threshold_4_reg;
+reg [14-1:0] adc_photon_threshold_5_reg;
+reg [14-1:0] adc_photon_threshold_6_reg;
+reg [14-1:0] adc_photon_threshold_7_reg;
+
+assign adc_photon_threshold_1 = adc_photon_threshold_1_reg;
+assign adc_photon_threshold_2 = adc_photon_threshold_2_reg;
+assign adc_photon_threshold_3 = adc_photon_threshold_3_reg;
+assign adc_photon_threshold_4 = adc_photon_threshold_4_reg;
+assign adc_photon_threshold_5 = adc_photon_threshold_5_reg;
+assign adc_photon_threshold_6 = adc_photon_threshold_6_reg;
+assign adc_photon_threshold_7 = adc_photon_threshold_7_reg;
 
 
 
-assign led_o = some_reg[8-1:0];
+assign led_o = led_reg[8-1:0];
 
 //---------------------------------------------------------------------------------
 //
@@ -58,12 +82,26 @@ assign led_o = some_reg[8-1:0];
 
 always @(posedge clk_i) begin
    if (rstn_i == 1'b0) begin
-      some_reg    <= 14'h0 ;
+      led_reg    <= 8'h0 ;
+      adc_photon_threshold_1_reg  <= 14'b0;
+      adc_photon_threshold_2_reg  <= 14'b0;
+      adc_photon_threshold_3_reg  <= 14'b0;
+      adc_photon_threshold_4_reg  <= 14'b0;
+      adc_photon_threshold_5_reg  <= 14'b0;
+      adc_photon_threshold_6_reg  <= 14'b0;
+      adc_photon_threshold_7_reg  <= 14'b0;
 
    end
    else begin
       if (sys_wen) begin
-         if (sys_addr[19:0]==20'h0)    some_reg  <= sys_wdata[14-1:0] ;
+         if (sys_addr[20-1:0]==20'h0)    led_reg  <= sys_wdata[8-1:0] ;
+         if (sys_addr[20-1:0]==20'h1)    adc_photon_threshold_1_reg  <= sys_wdata[14-1:0] ;
+         if (sys_addr[20-1:0]==20'h2)    adc_photon_threshold_2_reg  <= sys_wdata[14-1:0] ;
+         if (sys_addr[20-1:0]==20'h3)    adc_photon_threshold_3_reg  <= sys_wdata[14-1:0] ;
+         if (sys_addr[20-1:0]==20'h4)    adc_photon_threshold_4_reg  <= sys_wdata[14-1:0] ;
+         if (sys_addr[20-1:0]==20'h5)    adc_photon_threshold_5_reg  <= sys_wdata[14-1:0] ;
+         if (sys_addr[20-1:0]==20'h6)    adc_photon_threshold_6_reg  <= sys_wdata[14-1:0] ;
+         if (sys_addr[20-1:0]==20'h7)    adc_photon_threshold_7_reg  <= sys_wdata[14-1:0] ;
       end
    end
 end
@@ -78,9 +116,16 @@ if (rstn_i == 1'b0) begin
 end else begin
    _sys_err <= 1'b0 ;
 
-   casez (sys_addr[19:0])
-      20'h0   : begin _sys_ack <= sys_en;         _sys_rdata <= {{32-14{1'b0}}, some_reg}           ; end 
-     default : begin _sys_ack <= sys_en;          _sys_rdata <=  32'h0                              ; end
+   casez (sys_addr[20-1:0])
+      20'h0   : begin _sys_ack <= sys_en;         _sys_rdata <= {{32-8{1'b0}}, led_reg}              ; end
+      20'h1   : begin _sys_ack <= sys_en;         _sys_rdata <= {{32-14{1'b0}}, adc_photon_threshold_1_reg}              ; end
+      20'h2   : begin _sys_ack <= sys_en;         _sys_rdata <= {{32-14{1'b0}}, adc_photon_threshold_2_reg}              ; end
+      20'h3   : begin _sys_ack <= sys_en;         _sys_rdata <= {{32-14{1'b0}}, adc_photon_threshold_3_reg}              ; end
+      20'h4   : begin _sys_ack <= sys_en;         _sys_rdata <= {{32-14{1'b0}}, adc_photon_threshold_4_reg}              ; end
+      20'h5   : begin _sys_ack <= sys_en;         _sys_rdata <= {{32-14{1'b0}}, adc_photon_threshold_5_reg}              ; end
+      20'h6   : begin _sys_ack <= sys_en;         _sys_rdata <= {{32-14{1'b0}}, adc_photon_threshold_6_reg}              ; end
+      20'h7   : begin _sys_ack <= sys_en;         _sys_rdata <= {{32-14{1'b0}}, adc_photon_threshold_7_reg}              ; end
+      default : begin _sys_ack <= sys_en;          _sys_rdata <=  32'h0                              ; end
    endcase
 end
 
